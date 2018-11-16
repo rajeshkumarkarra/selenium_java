09c084fa515845065d3c56dd4d2480e32e14cc3f
rajeshkumarkarra <rajeshkumarkarra@outlook.com>
2018-11-16 18:50:04 +0530

    My first commit

---
+++ src/javaNotes/DataTypes.java
@@ -0,0 +1,19 @@
+package javaNotes;
+//DATA TYPES IN JAVA:
+	/*
+	 * Java has two categories of data:
+	 * Primitive data (e.g., number, character)
+	 * Object data (programmer created types)
+	 */
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+//BOOLEAN:
+	/*boolean data type represents only one bit of information either true or false . 
+	 * Values of type boolean are not converted implicitly or explicitly (with casts) to any other type. 
+	 * But the programmer can easily write conversion code.
+	 */
+	}
+
+}
